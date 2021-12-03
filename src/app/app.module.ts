e02e13f65a09e0e260f921279a269d0de7a3a08d
d-arocutipa <arocutipa17@gmail.com>
2021-12-02 21:21:32 -0500

    inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BodyComponent } from './body/body.component';
+import { FormComponent } from './form/form.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FormComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+interface Formulario {
+  nombres: string;
+  apellidos: string;
+  ciudad: string;
+  celular: number;
+  edad: number;
+  correo: string;
+  educacion: string;
+}
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: [ './form.component.css' ]
+})
+export class FormComponent implements OnInit {
+
+  model = new FormGroup({
+    nombres: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    apellidos: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    ciudad: new FormControl(),
+    celular:new FormControl(),
+    edad:new FormControl(),
+    correo: new FormControl(),
+    educacion: new FormControl(),
+  });
+  
+  constructor() {
+  
+  }
+
+  ngOnInit(): void {
+   
+  }
+
+  get f(){
+
+    return this.model.controls;
+
+  }
+
+  onSubmit() {
+
+  }
+}
